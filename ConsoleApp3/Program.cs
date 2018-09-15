413862483c41a1f5d173074a7b07cb36a6021261
pbalthazar <balthy55@gmail.com>
2018-09-15 08:38:32 -0400

    V1 server and client

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using USherbrooke.ServiceModel.Sondage;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+
+        static async Task<IList<Poll>> GetSondages()
+        {
+            IList<Poll> result = null;
+ 
+            HttpResponseMessage response = await client.GetAsync("api/Sondage");
+            if (response.IsSuccessStatusCode)
+            {
+                result = await response.Content.ReadAsAsync<IList<Poll>>();
+            }
+            return result;
+        }
+
+        static async Task<PollQuestion> GetPollQuestion(int userId, int pollId, int currentQuestionId)
+        {
+            PollQuestion result = null;
+
+            HttpResponseMessage response = await client.GetAsync($"api/Sondage?userId={userId}&pollId={pollId}&currentQuestionId={currentQuestionId}");
+            if (response.IsSuccessStatusCode)
+            {
+                result = await response.Content.ReadAsAsync<PollQuestion>();
+            }
+            return result;
+        }
+
+        static async Task<bool> AnswerQuestion(int userId, PollQuestion question)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                $"api/Sondage?userId={userId}", question);
+            response.EnsureSuccessStatusCode();
+
+            bool succes = await response.Content.ReadAsAsync<bool>();
+            return succes;
+        }
+
+        static void ShowSondages(IList<Poll> sondages)
+        {
+            Console.WriteLine("Listing sondages...");
+            foreach (Poll sondage in sondages)
+            {
+                Console.WriteLine($"Id: {sondage.Id} \t Description: {sondage.Description}");
+            }
+        }
+
+        static void ShowPollQuestion(PollQuestion question)
+        {
+            if (question != null)
+            {
+                Console.WriteLine("The next question is: ");
+                Console.WriteLine($"{question.QuestionId}: {question.Text}");
+            }
+        }
+
+        static void Main()
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task RunAsync()
+        {
+            // Update port # in the following line.
+            client.BaseAddress = new Uri("http://localhost:64636/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                // Get the poll
+                IList<Poll> sondages = null;
+                sondages = await GetSondages();
+                ShowSondages(sondages);
+                
+                string selectedSondage = Console.ReadLine();
+                int pollId = -1;
+
+                while (!Int32.TryParse(selectedSondage, out pollId)  || sondages.FirstOrDefault(x => x.Id == pollId) == null)
+                {
+                    Console.WriteLine("Choose a valid number...! Try again");
+                    pollId = -1;
+                    selectedSondage = Console.ReadLine();                    
+                }
+                Console.WriteLine(selectedSondage);
+
+                PollQuestion question = await GetPollQuestion(1, pollId, -1);
+                string questionAnswer = string.Empty;
+
+                while (question != null)
+                {
+                    ShowPollQuestion(question);
+                    questionAnswer = Console.ReadLine();
+                    question.Text = questionAnswer;
+
+                    await AnswerQuestion(1, question);
+                    question = await GetPollQuestion(1, pollId, question.QuestionId);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WebApplication3/Services/SondageService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using USherbrooke.ServiceModel.Sondage;
+
+namespace WebApplication3.Services
+{
+    public class SondageService : ISondageService
+    {
+        private SondageRepository sondageRepository;
+
+        public SondageService()
+        {
+            sondageRepository = new SondageRepository();
+        }
+
+        public int Connect()
+        {
+            throw new NotImplementedException();
+        }
+        public IList<Poll> GetAvailablePolls(int userId)
+        {
+            return sondageRepository.GetSondages();
+        }
+
+        public PollQuestion GetNextQuestion(int userId, int pollId, int currentQuestionId)
+        {
+            return sondageRepository.GetNextQuestion(pollId, currentQuestionId);
+        }
+
+        public bool SaveAnswer(int userId, PollQuestion question)
+        {
+            return sondageRepository.SaveQuestionAnswer(userId, question);
+        }
+    }
+}
