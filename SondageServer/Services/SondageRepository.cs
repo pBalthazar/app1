f0e5c50e731364072ff6ca4242b27cdfb7d99f2b
BenB-M <Benjamin.Bolduc-Meilleur@USherbrooke.ca>
2018-09-16 10:01:25 -0400

    Project framework changed from .net framework to .net core

---
+++ SondageServer/Services/SondageRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using USherbrooke.ServiceModel.Sondage;
+
+namespace SondageServer.Services
+{
+    public class SondageRepository
+    {
+        private SimpleSondageDAO sondage;
+
+        public SondageRepository()
+        {
+            sondage = new SimpleSondageDAO();
+        }
+
+        public IList<Poll> GetSondages()
+        {
+            return sondage.GetAvailablePolls();
+        }
+
+        public PollQuestion GetNextQuestion(int pollId, int currentQuestionId)
+        {
+            return sondage.GetNextQuestion(pollId, currentQuestionId);
+        }
+
+        public bool SaveQuestionAnswer(int userId, PollQuestion question)
+        {
+            sondage.SaveAnswer(userId, question);
+            return true;
+        }
+    }
+}
