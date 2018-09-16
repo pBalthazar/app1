8808a476248e4964ad9384ae670efc6bafa02e89
pbalthazar <balthy55@gmail.com>
2018-09-16 11:34:23 -0400

    Delete everything

--- WebApplication3/Controllers/RequireHttpsOrCloseAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Web.Mvc;
-
-public class RequireHttpsOrCloseAttribute : RequireHttpsAttribute
-{
-    protected override void HandleNonHttpsRequest(AuthorizationFilterContext filterContext)
-    {
-        filterContext.Result = new StatusCodeResult(400);
-    }
-}
