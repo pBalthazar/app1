68ac2155a697a83cab671d54b48daf6d401e8e1d
pbalthazar <balthy55@gmail.com>
2018-09-17 17:03:54 -0400

    Authentification v1

---
+++ SondageServer/Services/LoginService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using USherbrooke.ServiceModel.Sondage;
+
+namespace SondageServer.Services
+{
+    public class LoginService
+    {
+        private LoginRepository loginRepository;
+
+        public LoginService()
+        {
+            loginRepository = new LoginRepository();
+        }
+
+        public int? Connect(SondageUser user)
+        {
+            return loginRepository.GetUserId(user);
+        }
+    }
+}
