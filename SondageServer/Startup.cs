bebcd66bb639a8a5af8f403501f5c41f74070d77
BenB-M <Benjamin.Bolduc-Meilleur@USherbrooke.ca>
2018-09-17 17:01:41 -0400

    ratelimit nuget added

--- SondageServer/Startup.cs
+++ SondageServer/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using SondageServer.Services;
+using AspNetCoreRateLimit;
 
 namespace SondageServer
 {
@@ -27,11 +28,32 @@ namespace SondageServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Middleware for the api key authentication
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddAuthentication(options =>
             {
                 options.DefaultScheme= "Custom Scheme";
             }).AddCustomAuthentication("Custom Scheme", "My Custom Scheme", o => { });
+
+            //Middleware for the throttling
+            // needed to load configuration from appsettings.json
+            services.AddOptions();
+
+            // needed to store rate limit counters and ip rules
+            services.AddMemoryCache();
+
+            //load general configuration from appsettings.json
+            services.Configure<IpRateLimitOptions>(Configuration.GetSection("IpRateLimiting"));
+
+            //load ip rules from appsettings.json
+            services.Configure<IpRateLimitPolicies>(Configuration.GetSection("IpRateLimitPolicies"));
+
+            // inject counter and rules stores
+            services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+            services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+
+            // Add framework services.
+            //services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +72,7 @@ namespace SondageServer
                 app.UseHsts();
             }
 
+            app.UseIpRateLimiting();
             app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseMvc();
